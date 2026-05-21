e0bb7f4e684aee0858150507640050feea82a80f
MathieuGug <mathieu.guglielmino@gmail.com>
2026-05-21 04:52:56 +0200

    tweak(eval/hero): much bigger red blocked-marks on plates

--- evaluation-agentique/gruyere-hero.js
+++ evaluation-agentique/gruyere-hero.js
@@ -193,7 +193,7 @@ function buildMarkSystem(maxCount) {
     vertexShader: `
       void main() {
         vec4 mvPosition = modelViewMatrix * vec4(position, 1.0);
-        gl_PointSize = 42.0 * (1.0 / -mvPosition.z);
+        gl_PointSize = 110.0 * (1.0 / -mvPosition.z);
         gl_Position = projectionMatrix * mvPosition;
       }
     `,
